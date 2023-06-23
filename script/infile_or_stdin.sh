14799c428896b896ec5a5c88b6a4a95fa4107bb8
Dave Tang <davetingpongtang@gmail.com>
2023-06-23 10:04:41 +0900

    Read from file or stream

---
+++ script/infile_or_stdin.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# Written by Dave Tang
+# Year 2023
+#
+set -euo pipefail
+
+usage(){
+>&2 cat << EOF
+   Usage
+
+            $ $0 (<infile> | <stdin>)"
+
+   Examples
+
+            Read from a file
+
+            $ $0 /etc/resolv.conf
+
+            Read from STDIN
+
+            $ whoami | $0 -
+
+EOF
+exit 1
+}
+
+if [[ $# -lt 1 ]]; then
+  usage
+fi
+
+input=$1
+cat ${input}
+
+exit 0
