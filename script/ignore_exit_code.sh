fd155b6ee9b6d523cadae0320cab4457c075f477
Dave Tang <davetingpongtang@gmail.com>
2023-06-23 09:44:58 +0900

    Working example

--- script/ignore_exit_code.sh
+++ script/ignore_exit_code.sh
@@ -6,9 +6,16 @@ set -o errexit
 # code > 0 when it has no matches
 # grep nothing README.md
 
-# use the following to ignore a "failing" command
-if ! grep nothing README.md ; then
-   >&2 echo "Failure ignored"
+script_dir=$(realpath $(dirname $0))
+infile=${script_dir}/../README.md
+
+# use the following construct to ignore a "failing" command
+if ! grep -q not_going_to_find_this ${infile} ; then
+   >&2 echo "Failure ignored; continuing..."
+fi
+
+if grep -q e ${infile} ; then
+   >&2 echo "The letter [e] was found in ${infile}"
 fi
 
 >&2 echo "Done"
