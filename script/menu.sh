949ba083a29bf78b092b7fc280cdfd6b3367ed02
Dave Tang <davetingpongtang@gmail.com>
2024-04-18 19:23:54 +0900

    Find select menu

--- script/menu.sh
+++ script/menu.sh
@@ -1,34 +1,30 @@
 #!/usr/bin/env bash
 #
-# A simple menu
+# A simple menu using select
 #
 
 set -euo pipefail
 
-choices=($(echo {0..9}))
-num_choices=${#choices[*]}
-
-check_choice(){
-   if [[ ${1} =~ [a-zA-Z]+ ]]; then
-      >&2 echo Please enter a number
-   elif [[ ${1} -gt ${#choices[*]} ]]; then
-      >&2 echo Please enter a number between 1 - ${num_choices}
-   else
-      break
-   fi
-}
+CHOICES=($(echo {0..9}))
+NUM_CHOICES=${#CHOICES[*]}
 
 menu(){
    >&2 echo -e "\nPlease make a selection:\n"
-   select selected in ${choices[@]}
+   select SELECTED in ${CHOICES[@]}
    do
-      check_choice ${REPLY}
+      if [[ ${REPLY} =~ [a-zA-Z]+ ]]; then
+         >&2 echo Please enter a number
+      elif [[ ${REPLY} -gt ${#CHOICES[*]} ]]; then
+         >&2 echo Please enter a number between 1 - ${NUM_CHOICES}
+      else
+         break
+      fi
    done
 
-   >&2 echo -e "\nYou have selected ${selected}\n"
-   read -p "Do you want to make another selection: y/N? " continue
+   >&2 echo -e "\nYou have entered ${REPLY} which corresponds to ${SELECTED}\n"
+   read -p "Do you want to make another selection: y/N? " CONTINUE
 
-   if [[ ${continue} == y ]]; then
+   if [[ ${CONTINUE} == y ]]; then
       menu
    else
       exit
